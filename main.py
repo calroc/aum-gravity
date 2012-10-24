13f9efea147c8aabfd12ea2a7212dbbf21525508
Simon Forman <simonf@gablit.com>
2012-10-24 13:59:14 -0700

    Templates.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
-    return 'Index Page <a href="' + url_for('hello') + '">hello!</a>'
+    return render_template(
+      'hello.html',
+      title='This is Certainly a Title',
+      hello_url=url_for('hello'),
+      )
+
 
 @app.route('/hello')
 def hello():
     return "Hello World!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
