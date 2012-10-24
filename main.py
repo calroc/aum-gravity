b405439ccc1c724aacebda3377022b4643b89a82
Simon Forman <simonf@gablit.com>
2012-10-24 13:47:57 -0700

    Slightly more elaborate "Hello World"

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, url_for
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return 'Index Page <a href="' + url_for('hello') + '">hello!</a>'
+
+@app.route('/hello')
 def hello():
     return "Hello World!"
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
