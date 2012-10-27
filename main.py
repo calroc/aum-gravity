c870dd3bb6c8d929f96d92498ec0c98028758449
Simon Forman <forman.simon@gmail.com>
2012-10-26 19:14:34 -0700

    Add a bunch of JS goodies and some default style
    jquery
    knockout
    
    and CSS et. al. from http://goldilocksapproach.com/

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template(
-      'hello.html',
+      'index.html',
       title='This is Certainly a Title',
       hello_url=url_for('hello'),
       )
